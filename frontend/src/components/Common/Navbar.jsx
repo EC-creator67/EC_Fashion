cb00d612c69156f347cc0f2ee35363be157bb444
EC-creator67 <ettorecox@gmail.com>
2026-09-18 17:22:50 -0400

    changed name on navbar

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
         {/* Left - Logo */}
         <div>
           <Link to="/" className="text-2xl font-medium">
-            EC-Fashion
+            EC_Fashion
           </Link>
         </div>
         {/* Center - Navigation Link */}
